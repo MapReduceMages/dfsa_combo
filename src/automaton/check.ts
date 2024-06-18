d681474899608598d22e71b825f1466300f7aaa9
trixky <mabois@student.42.fr>
2024-06-18 17:00:11 +0200

    [Test] automaton: add test on the check functions

--- src/automaton/check.ts
+++ src/automaton/check.ts
@@ -9,6 +9,7 @@ export const actionAreComboCheck = (gameset: Readonly<GameSet>) => (actions: Act
 
 // actionsAreStartOfComboCheck checks if the actions are the start of a combo from the gameset
 export const actionsAreStartOfComboCheck = (gameset: Readonly<GameSet>) => (actions: Actions): boolean => {
+    if (actions.isEmpty()) return false;
     return gameset.combos
         .some(combo => actions
             .every((action, index) => action === combo.actions.get(index)))
