42caff83958ba1acff00593c829e22e4eb8b4dd8
trixky <mabois@student.42.fr>
2024-06-18 17:00:21 +0200

    [Test] automaton: add test on the state functions

--- src/automaton/state.test.ts
+++ src/automaton/state.test.ts
@@ -2,6 +2,9 @@ import type { State, Actions } from '../models/state';
 import { actionsToState, stateToActions, cleanState } from './state'
 import config from "../../config.json"
 import Immutable from 'immutable';
+import GameSet from '../models/game_set';
+import KeyMap from '../models/key_map';
+import Combo from '../models/combo';
 
 describe('actionsToState', () => {
     const validTests = [
@@ -12,7 +15,7 @@ describe('actionsToState', () => {
     ];
 
     for (const test of validTests) {
-        const state: State = test.join(config.splitter.keyMap);
+        const state: State = test.join(config.splitter.comboKeyMap);
         it(`[${state}]`, () => {
             expect(actionsToState(test)).toBe(state);
         });
@@ -28,13 +31,67 @@ describe('stateToActions', () => {
     ];
 
     for (const test of validTests) {
-        const state: State = test.join(config.splitter.keyMap);
+        const state: State = test.join(config.splitter.comboKeyMap);
         it(`[${state}]`, () => {
             expect(stateToActions(state).equals(test)).toBeTruthy();
         });
     }
 })
 
-// describe('cleantate', () => {
-    // TODO
-// })
+describe('cleanState', () => {
+    const gameset: GameSet = <GameSet>{
+        keyMaps: Immutable.List<KeyMap>([
+            { key: 'a', action: 'ACTION_A' },
+            { key: 'b', action: 'ACTION_B' },
+            { key: 'c', action: 'ACTION_C' },
+            { key: 'l', action: 'ACTION_LONG_ACTION_LONG_ACTION_LONG_ACTION_LONG_ACTION' },
+        ]),
+        combos: Immutable.List<Combo>([
+            { name: "COMBO_AB", actions: Immutable.List(['ACTION_A', 'ACTION_B']) },
+            { name: "COMBO_BA", actions: Immutable.List(['ACTION_B', 'ACTION_A']) },
+            { name: "COMBO_CAB", actions: Immutable.List(['ACTION_C', 'ACTION_A', 'ACTION_B']) },
+        ]),
+    };
+
+    const validTests = [
+        {
+            actions: Immutable.List([]),
+            expected: "",
+        },
+        {
+            actions: Immutable.List(['ACTION_A']),
+            expected: Immutable.List(['ACTION_A']).join(config.splitter.comboKeyMap),
+        },
+        {
+            actions: Immutable.List(['ACTION_A', 'ACTION_A']),
+            expected: Immutable.List(['ACTION_A']).join(config.splitter.comboKeyMap),
+        },
+        {
+            actions: Immutable.List(['ACTION_C', 'ACTION_B', 'ACTION_A']),
+            expected: Immutable.List(['ACTION_B', 'ACTION_A']).join(config.splitter.comboKeyMap),
+        },
+        {
+            actions: Immutable.List(['ACTION_B', 'ACTION_C', 'ACTION_B', 'ACTION_A']),
+            expected: Immutable.List(['ACTION_B', 'ACTION_A']).join(config.splitter.comboKeyMap),
+        },
+        {
+            actions: Immutable.List(['ACTION_C', 'ACTION_A',]),
+            expected: Immutable.List(['ACTION_C', 'ACTION_A']).join(config.splitter.comboKeyMap),
+        },
+        {
+            actions: Immutable.List(['ACTION_C', 'ACTION_C', 'ACTION_A',]),
+            expected: Immutable.List(['ACTION_C', 'ACTION_A']).join(config.splitter.comboKeyMap),
+        },
+        {
+            actions: Immutable.List(['ACTION_B', 'ACTION_C', 'ACTION_C', 'ACTION_A',]),
+            expected: Immutable.List(['ACTION_C', 'ACTION_A']).join(config.splitter.comboKeyMap),
+        },
+    ];
+
+    for (const test of validTests) {
+        it(`[${test.actions.join(config.splitter.comboKeyMap)}]`, () => {
+            const state = actionsToState(test.actions);
+            expect(cleanState(gameset)(test.actions)).toBe(test.expected);
+        });
+    }
+})
